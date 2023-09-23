c4ee800f33d42757381f4089fa639e73a3494ac0
liktao <gdutcs2@163.com>
2023-09-23 22:39:33 +0800

    实现GoosItem组件封装，达到复用

--- src/views/Home/components/HomeProduct.vue
+++ src/views/Home/components/HomeProduct.vue
@@ -3,7 +3,9 @@
 
   import { getGoodAPI } from "@/apis/home";
 
-  import { onMounted, ref } from "vue";
+import { onMounted, ref } from "vue";
+
+import GoodsItem from './GoodsItem.vue'
 
   const goodList = ref([]);
   const getGoods = async () => {
@@ -27,12 +29,7 @@
         </RouterLink>
         <ul class="goods-list">
           <li v-for="good in cate.goods" :key="good.id">
-            <RouterLink to="/" class="goods-item">
-              <img v-img-lazy="good.picture" src="" alt="" />
-              <p class="name ellipsis">{{ good.name }}</p>
-              <p class="desc ellipsis">{{ good.desc }}</p>
-              <p class="price">&yen;{{ good.price }}</p>
-            </RouterLink>
+            <GoodsItem :good="good"/>
           </li>
         </ul>
       </div>
@@ -127,41 +124,6 @@
         }
       }
 
-      .goods-item {
-        display: block;
-        width: 220px;
-        padding: 20px 30px;
-        text-align: center;
-        transition: all 0.5s;
-
-        &:hover {
-          transform: translate3d(0, -3px, 0);
-          box-shadow: 0 3px 8px rgb(0 0 0 / 20%);
-        }
-
-        img {
-          width: 160px;
-          height: 160px;
-        }
-
-        p {
-          padding-top: 10px;
-        }
-
-        .name {
-          font-size: 16px;
-        }
-
-        .desc {
-          color: #999;
-          height: 29px;
-        }
-
-        .price {
-          color: $priceColor;
-          font-size: 20px;
-        }
-      }
     }
   }
 </style>
