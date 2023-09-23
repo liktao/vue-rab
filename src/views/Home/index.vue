a40d4f159281d384d30935da0e3a9ab39119856b
liktao <gdutcs2@163.com>
2023-09-23 17:11:45 +0800

    home字体拆分以及分类数据渲染

--- src/views/Home/index.vue
+++ src/views/Home/index.vue
@@ -1,14 +1,21 @@
-<template>
-  <div class="testVar">我是Home</div>
-</template>
-
-<script>
-  export default {};
+<script setup>
+  import HomeCategory from "./components/HomeCategory.vue";
+  import HomeBanner from "./components/HomeBanner.vue";
+  import HomeNew from "./components/HomeNew.vue";
+  import HomeHot from "./components/HomeHot.vue";
+  import HomeProduct from "./components/HomeProduct.vue";
 </script>
 
+<template>
+  <div class="container">
+    <HomeCategory />
+    <HomeBanner />
+  </div>
+  <HomeNew />
+  <HomeHot />
+  <HomeProduct />
+</template>
+
 <style lang="scss" scoped>
-  .testVar {
-    // background-color: $priceColor;
-    height: 800px;
-  }
+
 </style>
