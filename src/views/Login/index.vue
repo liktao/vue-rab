b7784d0c6e41196596098123a875fce8b2627822
liktao <gdutcs2@163.com>
2023-09-24 23:30:57 +0800

    登录页表单校验

--- src/views/Login/index.vue
+++ src/views/Login/index.vue
@@ -1,5 +1,28 @@
-<script setup></script>
+<script setup>
+  import { ref } from "vue";
+  // 表单数据对象
+  const userInfo = ref({
+    account: "1311111111",
+    password: "123456",
+    agree: true,
+  });
 
+  // 规则数据对象
+  const rules = {
+    account: [{ required: true, message: "用户名不能为空" }],
+    password: [
+      { required: true, message: "密码不能为空" },
+      { min: 6, max: 24, message: "密码长度要求6-14个字符" },
+    ],
+    agree: [
+      {
+        validator: (rule, val, callback) => {
+          return val ? callback() : new Error("请先同意协议");
+        },
+      },
+    ],
+  };
+</script>
 <template>
   <div>
     <header class="login-header">
@@ -21,12 +44,12 @@
         </nav>
         <div class="account-box">
           <div class="form">
-            <el-form label-position="right" label-width="60px" status-icon>
-              <el-form-item label="账户">
-                <el-input />
+            <el-form label-position="right" :rules="rules" label-width="60px" status-icon>
+              <el-form-item prop="account" label="账户">
+                <el-input v-model="userInfo.account" />
               </el-form-item>
-              <el-form-item label="密码">
-                <el-input />
+              <el-form-item prop="password" label="密码">
+                <el-input  v-model="userInfo.password"/>
               </el-form-item>
               <el-form-item label-width="22px">
                 <el-checkbox size="large">
