51c400e1e0fa42a299daff873ffda93a9bb6e36c
liktao <gdutcs2@163.com>
2023-09-24 16:02:46 +0800

    二级分类面包屑导航实现

--- src/apis/category.js
+++ src/apis/category.js
@@ -6,3 +6,18 @@ export function getCategoryAPI(id) {
     params: { id },
   });
 }
+
+/**
+ * @description: 获取二级分类列表数据
+ * @param {*} id 分类id 
+ * @return {*}
+ */
+
+export const getCategoryFilterAPI = (id) => {
+  return httpInstance({
+    url:'/category/sub/filter',
+    params:{
+      id
+    }
+  })
+}
